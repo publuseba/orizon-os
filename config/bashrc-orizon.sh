45341312830bd855aeea32232070bbebbe06f902
publuseba <levtsindrenko1@gmail.com>
2026-07-15 18:09:22 +0300

    Update bashrc-orizon.sh

--- config/bashrc-orizon.sh
+++ config/bashrc-orizon.sh
@@ -1,7 +1,7 @@
 
 
-export ORIZON_VERSION="Beta 2/Fix 1"
-export ORIZON_BUILD="15-04-2026"
+export ORIZON_VERSION="Beta 2/Fix 2"
+export ORIZON_BUILD="15-07-2026"
 export ORIZON_LINK="https://taplink.cc/orizon"
 export CLICOLOR=1
 export TERM=xterm-256color
